6858294cb47992f97d09cce7ed3e378d8b399101
MiaNSlater <MS1646@canterbury.ac.uk>
2023-04-03 16:53:58 +0100

    Created tab page for Blackjack Form

--- BlackjackGame/Form1.Designer.cs
+++ BlackjackGame/Form1.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.blackjackTab = new System.Windows.Forms.TabControl();
+            this.gameTab = new System.Windows.Forms.TabPage();
+            this.rulesTab = new System.Windows.Forms.TabPage();
+            this.blackjackTab.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // blackjackTab
+            // 
+            this.blackjackTab.Controls.Add(this.gameTab);
+            this.blackjackTab.Controls.Add(this.rulesTab);
+            this.blackjackTab.Location = new System.Drawing.Point(0, -2);
+            this.blackjackTab.Name = "blackjackTab";
+            this.blackjackTab.SelectedIndex = 0;
+            this.blackjackTab.Size = new System.Drawing.Size(1141, 753);
+            this.blackjackTab.TabIndex = 0;
+            // 
+            // gameTab
+            // 
+            this.gameTab.Location = new System.Drawing.Point(4, 34);
+            this.gameTab.Name = "gameTab";
+            this.gameTab.Padding = new System.Windows.Forms.Padding(3);
+            this.gameTab.Size = new System.Drawing.Size(1133, 715);
+            this.gameTab.TabIndex = 0;
+            this.gameTab.Text = "Game";
+            this.gameTab.UseVisualStyleBackColor = true;
+            // 
+            // rulesTab
+            // 
+            this.rulesTab.Location = new System.Drawing.Point(4, 34);
+            this.rulesTab.Name = "rulesTab";
+            this.rulesTab.Padding = new System.Windows.Forms.Padding(3);
+            this.rulesTab.Size = new System.Drawing.Size(1133, 715);
+            this.rulesTab.TabIndex = 1;
+            this.rulesTab.Text = "Rules";
+            this.rulesTab.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1143, 750);
+            this.Controls.Add(this.blackjackTab);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Blackjack";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.blackjackTab.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private TabControl blackjackTab;
+        private TabPage gameTab;
+        private TabPage rulesTab;
     }
 }
