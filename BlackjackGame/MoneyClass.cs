13d0b3ffd4a7fe5f23a8586f5f120dfd371332a0
Tyler Wood <TW402@canterbury.ac.uk>
2023-04-17 00:31:38 +0100

    Added a section of my code. Need to add players and their cards.

---
+++ BlackjackGame/MoneyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackjackGame
+{
+    public class Money
+    {
+
+    }
+
+    public void setMoney()
+    {
+         int cashValue = 1000;
+    }
+
+    public void winScenario()
+    {
+        
+    }
+}
